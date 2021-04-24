34a47df0a68220fcaedefbf11638fe17823c6ea2
MJ Choi <1621139@gmail.com>
2021-04-25 06:16:34 +0900

    [UPDATE] comment out main method

--- CSharpBookStudy/Ch8/DefaultInplementation/MainApp.cs
+++ CSharpBookStudy/Ch8/DefaultInplementation/MainApp.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace CSharpBookStudy.Ch8.DefaultInplementation
-{
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-            ILogger logger = new ConsoleLogger();
-            logger.WriteLog("System UP");
-            logger.WriteError("System Fail");
+//namespace CSharpBookStudy.Ch8.DefaultInplementation
+//{
+//    class MainApp
+//    {
+//        static void Main(string[] args)
+//        {
+//            ILogger logger = new ConsoleLogger();
+//            logger.WriteLog("System UP");
+//            logger.WriteError("System Fail");
 
-            ConsoleLogger clogger = new ConsoleLogger();
-            clogger.WriteLog("System UP");
-        }
-    }
-}
+//            ConsoleLogger clogger = new ConsoleLogger();
+//            clogger.WriteLog("System UP");
+//        }
+//    }
+//}
