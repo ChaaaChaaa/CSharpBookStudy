2c8cc69bac01ed5e5192fe539739db9e37571f73
MJ Choi <1621139@gmail.com>
2021-04-25 02:55:38 +0900

    [UPDATE] comment out main method

--- CSharpBookStudy/ch7/StaticField/MainApp.cs
+++ CSharpBookStudy/ch7/StaticField/MainApp.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace CSharpBookStudy.ch7.StaticField
-{
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine($"Global.Count : {Global.Count}");
+//namespace CSharpBookStudy.ch7.StaticField
+//{
+//    class MainApp
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine($"Global.Count : {Global.Count}");
 
-            new ClassA();
-            new ClassA();
-            new ClassB();
-            new ClassB();
+//            new ClassA();
+//            new ClassA();
+//            new ClassB();
+//            new ClassB();
 
-            Console.WriteLine($"Global.Count : {Global.Count}");
+//            Console.WriteLine($"Global.Count : {Global.Count}");
 
-        }
-    }
-}
+//        }
+//    }
+//}
