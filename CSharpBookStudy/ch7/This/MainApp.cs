d3f8d0492a5071d8684e6cd9b89cbd3f90ac8558
MJ Choi <1621139@gmail.com>
2021-04-25 03:33:19 +0900

    [UPDATE] comment out main method

--- CSharpBookStudy/ch7/This/MainApp.cs
+++ CSharpBookStudy/ch7/This/MainApp.cs
@@ -1,29 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace CSharpBookStudy.ch7.This
-{
-    class MainApp
-    {
-        public static void Main(string[] args)
-        {
-            Employee MJ = new Employee();
-            MJ.SetName("MJ");
-            MJ.SetPosition("CEO");
-            Console.WriteLine($"{MJ.GetName()} : {MJ.GetPosition()}");
+//namespace CSharpBookStudy.ch7.This
+//{
+//    class MainApp
+//    {
+//        public static void Main(string[] args)
+//        {
+//            Employee MJ = new Employee();
+//            MJ.SetName("MJ");
+//            MJ.SetPosition("CEO");
+//            Console.WriteLine($"{MJ.GetName()} : {MJ.GetPosition()}");
 
-            ThisConstructor a = new ThisConstructor();
-            a.PrintFields();
-            Console.WriteLine();
+//            ThisConstructor a = new ThisConstructor();
+//            a.PrintFields();
+//            Console.WriteLine();
 
-            ThisConstructor b = new ThisConstructor(1);
-            b.PrintFields();
-            Console.WriteLine();
+//            ThisConstructor b = new ThisConstructor(1);
+//            b.PrintFields();
+//            Console.WriteLine();
 
-            ThisConstructor c = new ThisConstructor(10,20);
-            c.PrintFields();
-            Console.WriteLine();
-        }
-    }
-}
+//            ThisConstructor c = new ThisConstructor(10,20);
+//            c.PrintFields();
+//            Console.WriteLine();
+//        }
+//    }
+//}
