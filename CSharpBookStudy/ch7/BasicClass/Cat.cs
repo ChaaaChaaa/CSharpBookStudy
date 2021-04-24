6a739fb1eb3a6ba0f82798117997cb71229d82e0
MJ Choi <1621139@gmail.com>
2021-04-25 02:38:23 +0900

    [UPDATE] finalize method

--- CSharpBookStudy/ch7/BasicClass/Cat.cs
+++ CSharpBookStudy/ch7/BasicClass/Cat.cs
@@ -9,6 +9,17 @@ namespace CSharpBookStudy.ch7
         public string Name;
         public string Color;
 
+        public Cat(string _Name, string _Color)
+        {
+            Name = _Name;
+            Color = _Color;
+        }
+
+        ~Cat()
+        {
+            Console.WriteLine($"{Name} : 잘가");
+        }
+
         public void Meow()
         {
             Console.WriteLine($"{Name} : 야옹");
--- CSharpBookStudy/ch7/BasicClass/MainApp.cs
+++ CSharpBookStudy/ch7/BasicClass/MainApp.cs
@@ -6,15 +6,15 @@ namespace CSharpBookStudy.ch7
     {
         static void Main(string[] args)
         {
-            Cat kitty = new Cat();
-            kitty.Name = "키티";
-            kitty.Color = "White";
+            Cat kitty = new Cat("키티", "White");
+            //kitty.Name = "키티";
+            //kitty.Color = "White";
             kitty.Meow();
             Console.WriteLine($"{kitty.Name} : {kitty.Color}");
 
-            Cat nero = new Cat();
-            nero.Name = "네로";
-            nero.Color = "Black";
+            Cat nero = new Cat("네로","Black");
+            //nero.Name = "네로";
+            //nero.Color = "Black";
             nero.Meow();
             Console.WriteLine($"{nero.Name} : {nero.Color}");
         }
