1633e07cd4005e6983dcf2dd9d916d9318b817c4
MJ Choi <1621139@gmail.com>
2021-04-25 02:56:01 +0900

    [ADD] deep copy and shallow copy

---
+++ CSharpBookStudy/ch7/DeepCopy/DoDeepCopy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBookStudy.ch7.DeepCopy
+{
+    class DoDeepCopy
+    {
+        public int MyField1;
+        public int MyField2;
+
+        public DoDeepCopy DeepCopyFunction()
+        {
+            DoDeepCopy newDeepCopy = new DoDeepCopy();
+            newDeepCopy.MyField1 = this.MyField1;
+            newDeepCopy.MyField2 = this.MyField2;
+
+            return newDeepCopy;
+        }
+    }
+}
---
+++ CSharpBookStudy/ch7/DeepCopy/MainApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBookStudy.ch7.DeepCopy
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shallow Copy");
+            {
+                DoDeepCopy source = new DoDeepCopy();
+                source.MyField1 = 10;
+                source.MyField2 = 20;
+
+                DoDeepCopy target = source;
+                target.MyField2 = 30;
+
+                Console.WriteLine($"{source.MyField1},{source.MyField2}");
+                Console.WriteLine($"{target.MyField1},{target.MyField2}");
+            }
+
+            Console.WriteLine("Deep Copy");
+            {
+                DoDeepCopy source = new DoDeepCopy();
+                source.MyField1 = 10;
+                source.MyField2 = 20;
+
+                DoDeepCopy target = source.DeepCopyFunction();
+                target.MyField2 = 30;
+
+                Console.WriteLine($"{source.MyField1},{source.MyField2}");
+                Console.WriteLine($"{target.MyField1},{target.MyField2}");
+            }
+        }
+    }
+}
