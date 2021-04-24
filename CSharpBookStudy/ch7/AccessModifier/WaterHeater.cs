d560255f72648093df44d5fd6f705ea55eab4fad
MJ Choi <1621139@gmail.com>
2021-04-25 03:33:41 +0900

    [ADD] Access Modifier Classes

---
+++ CSharpBookStudy/ch7/AccessModifier/WaterHeater.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBookStudy.ch7.AccessModifier
+{
+    class WaterHeater
+    {
+        protected int temperature;
+        public void SetTemperature(int temperature)
+        {
+            if(temperature < -5 || temperature > 42)
+            {
+                throw new Exception("Out of temperature range");
+            }
+
+            this.temperature = temperature;
+        }
+
+        internal void TurnOnWater()
+        {
+            Console.WriteLine($"Turn on water : {temperature}");
+        }
+    }
+}
