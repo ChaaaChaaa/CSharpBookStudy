ccf37abc070179382f54af9cfc38c331d33b40af
MJ Choi <1621139@gmail.com>
2021-04-25 02:32:38 +0900

    [ADD] Basic Object Class

---
+++ CSharpBookStudy/ch7/BasicClass/Cat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBookStudy.ch7
+{
+    class Cat
+    {
+        public string Name;
+        public string Color;
+
+        public void Meow()
+        {
+            Console.WriteLine($"{Name} : 야옹");
+        }
+    }
+}
---
+++ CSharpBookStudy/ch7/BasicClass/MainApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharpBookStudy.ch7
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat();
+            kitty.Name = "키티";
+            kitty.Color = "White";
+            kitty.Meow();
+            Console.WriteLine($"{kitty.Name} : {kitty.Color}");
+
+            Cat nero = new Cat();
+            nero.Name = "네로";
+            nero.Color = "Black";
+            nero.Meow();
+            Console.WriteLine($"{nero.Name} : {nero.Color}");
+        }
+    }
+}
