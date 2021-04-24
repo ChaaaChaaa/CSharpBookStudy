ce23c20c6e1ea5acbc0338c1889618b033dedfb1
MJ Choi <1621139@gmail.com>
2021-04-25 04:00:12 +0900

    [UPDATE] comment out main method

--- CSharpBookStudy/ch7/AccessModifier/MainApp.cs
+++ CSharpBookStudy/ch7/AccessModifier/MainApp.cs
@@ -1,29 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace CSharpBookStudy.ch7.AccessModifier
-{
-    class MainApp
-    {
-        static void Main(String[] args)
-        {
-            try
-            {
-                WaterHeater heater = new WaterHeater();
-                heater.SetTemperature(20);
-                heater.TurnOnWater();
+//namespace CSharpBookStudy.ch7.AccessModifier
+//{
+//    class MainApp
+//    {
+//        static void Main(String[] args)
+//        {
+//            try
+//            {
+//                WaterHeater heater = new WaterHeater();
+//                heater.SetTemperature(20);
+//                heater.TurnOnWater();
 
-                heater.SetTemperature(-2);
-                heater.TurnOnWater();
+//                heater.SetTemperature(-2);
+//                heater.TurnOnWater();
 
-                heater.SetTemperature(50);
-                heater.TurnOnWater();
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
-    }
-}
+//                heater.SetTemperature(50);
+//                heater.TurnOnWater();
+//            }
+//            catch(Exception e)
+//            {
+//                Console.WriteLine(e.Message);
+//            }
+//        }
+//    }
+//}
