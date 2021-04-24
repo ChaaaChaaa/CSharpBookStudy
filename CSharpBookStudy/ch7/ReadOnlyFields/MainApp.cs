bf1ae38e25b0ad6413bc032ead7f68c8d51cfbae
MJ Choi <1621139@gmail.com>
2021-04-25 05:52:27 +0900

    [UPDATE] comment out main method

--- CSharpBookStudy/ch7/ReadOnlyFields/MainApp.cs
+++ CSharpBookStudy/ch7/ReadOnlyFields/MainApp.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace CSharpBookStudy.ch7.ReadOnlyFields
-{
-    class MainApp
-    {
-        static void Main(String[] args)
-        {
-            Configuration configuration = new Configuration(100, 10);
-        }
-    }
-}
+//namespace CSharpBookStudy.ch7.ReadOnlyFields
+//{
+//    class MainApp
+//    {
+//        static void Main(String[] args)
+//        {
+//            Configuration configuration = new Configuration(100, 10);
+//        }
+//    }
+//}
