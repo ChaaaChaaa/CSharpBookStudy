63be70407228d0a7741377f4d6645d10c84c3891
MJ Choi <1621139@gmail.com>
2021-04-25 02:45:38 +0900

    [UPDATE] comment out main method

--- CSharpBookStudy/ch7/BasicClass/MainApp.cs
+++ CSharpBookStudy/ch7/BasicClass/MainApp.cs
@@ -1,22 +1,22 @@
-﻿using System;
+﻿//using System;
 
-namespace CSharpBookStudy.ch7
-{
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-            Cat kitty = new Cat("키티", "White");
-            //kitty.Name = "키티";
-            //kitty.Color = "White";
-            kitty.Meow();
-            Console.WriteLine($"{kitty.Name} : {kitty.Color}");
+//namespace CSharpBookStudy.ch7
+//{
+//    class MainApp
+//    {
+//        static void Main(string[] args)
+//        {
+//            Cat kitty = new Cat("키티", "White");
+//            //kitty.Name = "키티";
+//            //kitty.Color = "White";
+//            kitty.Meow();
+//            Console.WriteLine($"{kitty.Name} : {kitty.Color}");
 
-            Cat nero = new Cat("네로","Black");
-            //nero.Name = "네로";
-            //nero.Color = "Black";
-            nero.Meow();
-            Console.WriteLine($"{nero.Name} : {nero.Color}");
-        }
-    }
-}
+//            Cat nero = new Cat("네로","Black");
+//            //nero.Name = "네로";
+//            //nero.Color = "Black";
+//            nero.Meow();
+//            Console.WriteLine($"{nero.Name} : {nero.Color}");
+//        }
+//    }
+//}
