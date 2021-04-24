f00b8c422ec0d9998fa1a57588b0d45583ebf27e
MJ Choi <1621139@gmail.com>
2021-04-25 03:22:36 +0900

    [UPDATE] comment out main method

--- CSharpBookStudy/ch7/DeepCopy/MainApp.cs
+++ CSharpBookStudy/ch7/DeepCopy/MainApp.cs
@@ -6,33 +6,33 @@ namespace CSharpBookStudy.ch7.DeepCopy
 {
     class MainApp
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Shallow Copy");
-            {
-                DoDeepCopy source = new DoDeepCopy();
-                source.MyField1 = 10;
-                source.MyField2 = 20;
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Shallow Copy");
+        //    {
+        //        DoDeepCopy source = new DoDeepCopy();
+        //        source.MyField1 = 10;
+        //        source.MyField2 = 20;
 
-                DoDeepCopy target = source;
-                target.MyField2 = 30;
+        //        DoDeepCopy target = source;
+        //        target.MyField2 = 30;
 
-                Console.WriteLine($"{source.MyField1},{source.MyField2}");
-                Console.WriteLine($"{target.MyField1},{target.MyField2}");
-            }
+        //        Console.WriteLine($"{source.MyField1},{source.MyField2}");
+        //        Console.WriteLine($"{target.MyField1},{target.MyField2}");
+        //    }
 
-            Console.WriteLine("Deep Copy");
-            {
-                DoDeepCopy source = new DoDeepCopy();
-                source.MyField1 = 10;
-                source.MyField2 = 20;
+        //    Console.WriteLine("Deep Copy");
+        //    {
+        //        DoDeepCopy source = new DoDeepCopy();
+        //        source.MyField1 = 10;
+        //        source.MyField2 = 20;
 
-                DoDeepCopy target = source.DeepCopyFunction();
-                target.MyField2 = 30;
+        //        DoDeepCopy target = source.DeepCopyFunction();
+        //        target.MyField2 = 30;
 
-                Console.WriteLine($"{source.MyField1},{source.MyField2}");
-                Console.WriteLine($"{target.MyField1},{target.MyField2}");
-            }
-        }
+        //        Console.WriteLine($"{source.MyField1},{source.MyField2}");
+        //        Console.WriteLine($"{target.MyField1},{target.MyField2}");
+        //    }
+        //}
     }
 }
