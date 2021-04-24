59d6f7c6a36080ad04cd7283aa047f3dd6469294
MJ Choi <1621139@gmail.com>
2021-04-25 05:59:18 +0900

    [UPDATE] comment out main method

--- CSharpBookStudy/Ch8/Interface/MainApp.cs
+++ CSharpBookStudy/Ch8/Interface/MainApp.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace CSharpBookStudy.Ch8.Interface
-{
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-            IFormattableLogger logger = new ConsoleLogger2();
-            logger.WriteLog("The world is not flat.");
-            logger.WriteLog("{0}+{1}={2}", 1, 2, 3);
-        }
-    }
-}
+//namespace CSharpBookStudy.Ch8.Interface
+//{
+//    class MainApp
+//    {
+//        static void Main(string[] args)
+//        {
+//            IFormattableLogger logger = new ConsoleLogger2();
+//            logger.WriteLog("The world is not flat.");
+//            logger.WriteLog("{0}+{1}={2}", 1, 2, 3);
+//        }
+//    }
+//}
