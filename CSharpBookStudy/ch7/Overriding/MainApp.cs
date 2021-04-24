0193e508cec7055d7f922ea2786b9efb34271dcf
MJ Choi <1621139@gmail.com>
2021-04-25 05:06:10 +0900

    [UPDATE] comment out main method

--- CSharpBookStudy/ch7/Overriding/MainApp.cs
+++ CSharpBookStudy/ch7/Overriding/MainApp.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace CSharpBookStudy.ch7.Overriding
-{
-    class MainApp : ArmorSuite
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Creating ArmorSuite...");
-            ArmorSuite armorSuite = new ArmorSuite();
-            armorSuite.Initialize();
+//namespace CSharpBookStudy.ch7.Overriding
+//{
+//    class MainApp : ArmorSuite
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Creating ArmorSuite...");
+//            ArmorSuite armorSuite = new ArmorSuite();
+//            armorSuite.Initialize();
 
-            Console.WriteLine("\nCreating IronMan...");
-            ArmorSuite ironMan = new IronMan();
-            ironMan.Initialize();
+//            Console.WriteLine("\nCreating IronMan...");
+//            ArmorSuite ironMan = new IronMan();
+//            ironMan.Initialize();
 
-            Console.WriteLine("\nCreating WarMaching...");
-            ArmorSuite warMachine = new WarMachine();
-            warMachine.Initialize();
-        }
-    }
-}
+//            Console.WriteLine("\nCreating WarMaching...");
+//            ArmorSuite warMachine = new WarMachine();
+//            warMachine.Initialize();
+//        }
+//    }
+//}
