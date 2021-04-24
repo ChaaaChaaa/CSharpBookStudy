135c32da93f9a13ba05ed03e37bf132bb6f9cd4e
MJ Choi <1621139@gmail.com>
2021-04-25 05:06:40 +0900

    [ADD] Read onlt fields and const

---
+++ CSharpBookStudy/ch7/ReadOnlyFields/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBookStudy.ch7.ReadOnlyFields
+{
+    class Configuration
+    {
+        const int testValue = 999;
+        private readonly int min;
+        private readonly int max;
+
+        public Configuration(int v1, int v2)
+        {
+            min = v1;
+            min = v2;
+        }
+
+        //public void ChangeMax(int newMax)
+        //{
+        //    max = newMax;
+        //}
+    }
+}
---
+++ CSharpBookStudy/ch7/ReadOnlyFields/MainApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBookStudy.ch7.ReadOnlyFields
+{
+    class MainApp
+    {
+        static void Main(String[] args)
+        {
+            Configuration configuration = new Configuration(100, 10);
+        }
+    }
+}
