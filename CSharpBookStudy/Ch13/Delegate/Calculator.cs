7116d51320cdd03376be34f19c46bce13f1bb923
MJ Choi <1621139@gmail.com>
2021-04-28 02:20:02 +0900

    [ADD] Delegate

---
+++ CSharpBookStudy/Ch13/Delegate/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBookStudy.Ch13.Delegate
+{    
+    class Calculator
+    {
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ CSharpBookStudy/Ch13/Delegate/MainApp.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace CSharpBookStudy.Ch13.Delegate
+//{
+//    class MainApp
+//    {
+//        delegate int MyDelegate(int a, int b);
+//        static void Main(string[] args)
+//        {
+//            Calculator calculator = new Calculator();
+//            MyDelegate Callback;
+
+//            Callback = new MyDelegate(calculator.Plus);
+//            Console.WriteLine(Callback(3, 4));
+
+//            Callback = new MyDelegate(calculator.Minus);
+//            Console.WriteLine(Callback(7, 4));
+//        }
+//    }
+//}
