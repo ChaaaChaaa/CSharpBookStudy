136e3e13a2663792326c887095009fcf11c48de7
MJ Choi <1621139@gmail.com>
2021-04-28 02:18:02 +0900

    [ADD] Initializing Array

---
+++ CSharpBookStudy/Ch10/InitializingArray/ArrayInit.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace CSharpBookStudy.Ch10.InitializingArray
+//{
+//    class ArrayInit
+//    {
+//        static void Main(string[] args)
+//        {
+//            string[] arr1 = new string[3] { "안녕", "Hello", "Halo" };
+
+//            Console.WriteLine("arr1...");
+//            foreach (string greeting in arr1)
+//            {
+//                Console.WriteLine($"{greeting}");
+//            }
+
+//            string[] arr2 = new string[] { "안녕", "Hello", "Halo" };
+//            Console.WriteLine("arr2...");
+//            foreach (string greeting in arr2)
+//            {
+//                Console.WriteLine($"{greeting}");
+//            }
+
+//            string[] arr3 = { "안녕", "Hello", "Halo" };
+//            Console.WriteLine("arr3...");
+//            foreach (string greeting in arr3)
+//            {
+//                Console.WriteLine($"{greeting}");
+//            }
+//        }
+//    }
+//}
