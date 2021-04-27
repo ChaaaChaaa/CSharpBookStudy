1391e1d9bf69a6eb07ac2ad00cf6d33639fb0f6b
MJ Choi <1621139@gmail.com>
2021-04-28 02:17:37 +0900

    [ADD] Multiple Array

---
+++ CSharpBookStudy/Ch10/DerivedFromArray/MainApp.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace CSharpBookStudy.Ch10.DerivedFromArray
+//{
+//    class MainApp
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] arr = new int[] { 10, 30, 20, 7, 1 };
+//            Console.WriteLine($"Type Of array : {arr.GetType()}");
+//            Console.WriteLine($"Base type Of array : {arr.GetType().BaseType}");
+//        }
+//    }
+//}
