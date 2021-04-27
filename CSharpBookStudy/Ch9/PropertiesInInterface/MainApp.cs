34a7b96f35260a3ec9ba7806608c25db5e5d7256
MJ Choi <1621139@gmail.com>
2021-04-28 00:33:27 +0900

    [UPDATE] comment out

--- CSharpBookStudy/Ch9/PropertiesInInterface/MainApp.cs
+++ CSharpBookStudy/Ch9/PropertiesInInterface/MainApp.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace CSharpBookStudy.Ch9.PropertiesInInterface
-{
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-            NamedValue name = new NamedValue()
-            { Name = "이름", Value = "박상현" };
+//namespace CSharpBookStudy.Ch9.PropertiesInInterface
+//{
+//    class MainApp
+//    {
+//        static void Main(string[] args)
+//        {
+//            NamedValue name = new NamedValue()
+//            { Name = "이름", Value = "박상현" };
 
-            NamedValue height = new NamedValue()
-            { Name = "키", Value = "177Cm" };
+//            NamedValue height = new NamedValue()
+//            { Name = "키", Value = "177Cm" };
 
-            NamedValue weight = new NamedValue()
-            { Name = "몸무게", Value = "90Kg" };
+//            NamedValue weight = new NamedValue()
+//            { Name = "몸무게", Value = "90Kg" };
 
-            Console.WriteLine($"{name.Name} : {name.Value}");
-            Console.WriteLine($"{height.Name} : {height.Value}");
-            Console.WriteLine($"{weight.Name} : {weight.Value}");
-        }
-    }
-}
+//            Console.WriteLine($"{name.Name} : {name.Value}");
+//            Console.WriteLine($"{height.Name} : {height.Value}");
+//            Console.WriteLine($"{weight.Name} : {weight.Value}");
+//        }
+//    }
+//}
