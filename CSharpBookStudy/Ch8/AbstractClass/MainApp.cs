7212af757166a5e22271143f63c1cfa264f1c623
MJ Choi <1621139@gmail.com>
2021-04-27 23:24:37 +0900

    [UPDATE] comment out

--- CSharpBookStudy/Ch8/AbstractClass/MainApp.cs
+++ CSharpBookStudy/Ch8/AbstractClass/MainApp.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace CSharpBookStudy.Ch8.AbstractClass
-{
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-            AbstractBase obj = new Derived();
-            obj.AbstractMethodA();
-            obj.PublicMethodA();
-        }
-    }
-}
+//namespace CSharpBookStudy.Ch8.AbstractClass
+//{
+//    class MainApp
+//    {
+//        static void Main(string[] args)
+//        {
+//            AbstractBase obj = new Derived();
+//            obj.AbstractMethodA();
+//            obj.PublicMethodA();
+//        }
+//    }
+//}
