8dc939c2b0b88c91a80bcc775f5f940a0ff539e7
MJ Choi <1621139@gmail.com>
2021-04-28 00:16:00 +0900

    [UPDATE] comment out

--- CSharpBookStudy/Ch9/ConstructorWithProperty/MainApp.cs
+++ CSharpBookStudy/Ch9/ConstructorWithProperty/MainApp.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace CSharpBookStudy.Ch9.ConstructorWithProperty
-{
-    class MainApp
-    {
-        public static void Main(string[] args)
-        {
-            BirthdayInfo3 birthdayInfo3 = new BirthdayInfo3()
-            {
-                Name = "미정",
-                Birthday = new DateTime(1991, 4, 30)
-            };
+//namespace CSharpBookStudy.Ch9.ConstructorWithProperty
+//{
+//    class MainApp
+//    {
+//        public static void Main(string[] args)
+//        {
+//            BirthdayInfo3 birthdayInfo3 = new BirthdayInfo3()
+//            {
+//                Name = "미정",
+//                Birthday = new DateTime(1991, 4, 30)
+//            };
 
-            Console.WriteLine($"Name : {birthdayInfo3.Name}");
-            Console.WriteLine($"Birthday : {birthdayInfo3.Birthday.ToShortDateString()}");
-            Console.WriteLine($"Age : {birthdayInfo3.Age}");
-        }
-    }
-}
+//            Console.WriteLine($"Name : {birthdayInfo3.Name}");
+//            Console.WriteLine($"Birthday : {birthdayInfo3.Birthday.ToShortDateString()}");
+//            Console.WriteLine($"Age : {birthdayInfo3.Age}");
+//        }
+//    }
+//}
