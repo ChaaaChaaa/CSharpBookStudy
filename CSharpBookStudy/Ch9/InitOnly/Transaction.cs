eb807f45b5be0f62f413b8d57acd50eff3f2535f
MJ Choi <1621139@gmail.com>
2021-04-28 02:15:57 +0900

    [UPDATE] comment out

--- CSharpBookStudy/Ch9/InitOnly/Transaction.cs
+++ CSharpBookStudy/Ch9/InitOnly/Transaction.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace CSharpBookStudy.Ch9.InitOnly
-{
-    class Transaction
-    {
-        public string From { get; init; }
-        public string To { get; init; }
-        public int Amount { get; init; }
+//namespace CSharpBookStudy.Ch9.InitOnly
+//{
+//    class Transaction
+//    {
+//        public string From { get; init; }
+//        public string To { get; init; }
+//        public int Amount { get; init; }
 
-        public override string ToString()
-        {
-            return $"{From,-10} ->{To,-10} : ${Amount}";
-        }
-    }
-}
+//        public override string ToString()
+//        {
+//            return $"{From,-10} ->{To,-10} : ${Amount}";
+//        }
+//    }
+//}
--- CSharpBookStudy/Ch9/PropertiesInAbstractClass/MainApp.cs
+++ CSharpBookStudy/Ch9/PropertiesInAbstractClass/MainApp.cs
@@ -1,30 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace CSharpBookStudy.Ch9.PropertiesInAbstractClass
-{
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-            Product product1 = new MyProduct()
-            {
-                ProductDate = new DateTime(2021, 4, 28)
-            };
+//namespace CSharpBookStudy.Ch9.PropertiesInAbstractClass
+//{
+//    class MainApp
+//    {
+//        static void Main(string[] args)
+//        {
+//            Product product1 = new MyProduct()
+//            {
+//                ProductDate = new DateTime(2021, 4, 28)
+//            };
 
-            Console.WriteLine("Product:{0}, Product DAte : {1}",
-                product1.SerialID,
-                product1.ProductDate);
+//            Console.WriteLine("Product:{0}, Product DAte : {1}",
+//                product1.SerialID,
+//                product1.ProductDate);
 
-            Product product2 = new MyProduct()
-            {
-                ProductDate = new DateTime(2021, 4, 28)
-            };
+//            Product product2 = new MyProduct()
+//            {
+//                ProductDate = new DateTime(2021, 4, 28)
+//            };
 
-            Console.WriteLine("Product : {0}, Product Date :{1}",
-                product2.SerialID,
-                product2.ProductDate);
-        }
-    }
-}
+//            Console.WriteLine("Product : {0}, Product Date :{1}",
+//                product2.SerialID,
+//                product2.ProductDate);
+//        }
+//    }
+//}
